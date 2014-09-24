b0bed21000569ed7df1acf6f4b205faa9f82e87a
Scott Butcher <scottcbutcher63@gmail.com>
2014-09-24 17:58:12 -0400

    Autocomplete now requres 3 letters

--- public/javascripts/tabcomplete.js
+++ public/javascripts/tabcomplete.js
@@ -77,8 +77,12 @@
 			words = [];
 			
 			// Check for matches if the current word is the last word.
-			if (self[0].selectionStart == input.length
-				&& word.length) {
+			//if (self[0].selectionStart == input.length
+			//	&& word.length) {
+			console.log(self)
+			console.log(self[0].selectionStart)
+			console.log(input)
+			if ( /[^ ][^ ][^ ]+$/.test(input) ) {
 				if (typeof args === "function") {
 					// If the user supplies a function, invoke it
 					// and keep the result.
