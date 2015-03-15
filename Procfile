cf46aa9b962fbd682fdfbf71289172431c666407
ElysianTail <elysian@sleepychat.com>
2015-03-15 16:01:15 +0100

    Procfile Fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./start.sh
+web: node app.js
--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-clear;
-until node app.js; do
-    echo "Server crashed with exit code $?.  Respawning.." >&2
-    sleep 1
-done
