cf9cf36e9f2a1aa4763bf4ba9f871ad05d0d0a34
ElysianTail <elysian@sleepychat.com>
2015-09-22 23:37:30 +0900

    Keen.IO Integration
    Added Keen.IO, an analyitics module.
    Keen will report userbase every 5 minutes.
    Modified stats route to direct to stats view.
    Stats view renders a graph comparing total users vs active, max and avg.

--- routes/stats.js
+++ routes/stats.js
@@ -3,7 +3,16 @@ var router = express.Router();
 
 router.get('/', function(req, res)
 {
-	res.send('This feature has not yet been implemented.');
+    var isNight = req.cookies.theme == "night";
+	res.render('stats', {
+        logoType: (isNight ? "night" : "day"),
+        nightStyle: (isNight ? "/stylesheets/night/common.css" : ""),
+        statsID: process.env.KEEN_PROJECT_ID,
+        statsKey: process.env.KEEN_READ_KEY,
+        statsInterval: "daily",
+        statsTimeframeNumber: 7,
+        statsTimeframeScale: "days"
+    });
 });
 
 module.exports = router;
